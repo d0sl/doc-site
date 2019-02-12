e1b1f45dd98433e67e308d6eb68285c9ee40dc5a
Peter Matyukov <kuruhuru@gmail.com>
2019-02-12 11:37:59 +0700

    Fix search on server: added -b option to deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 rem Build the project. if using a theme, replace with `hugo -t <YOURTHEME>`
-hugo
+hugo -b https://d0sl.github.io/
 
 rem Go To Public folder
 cd public
